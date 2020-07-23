3f40cddaaf08b702de0132f2d69c8fcbbecaaee1
James Monks <J.monks23@gmail.com>
2020-07-23 15:48:32 +0100

    MVP finished

--- lab_events_start/js/app.js
+++ lab_events_start/js/app.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
+
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleFormSubmit);
+
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = "Delete All";
+  const body = document.querySelector('body')
+  body.appendChild(deleteButton);
+  
 })
+
+  const handleFormSubmit = function(event) {
+    event.preventDefault();
+    const form = document.querySelector('#new-item-form');
+    const resultList = document.querySelector('#reading-list');
+    const newListItem = document.createElement('li');
+    newListItem.classList.add('list-item');
+    newListItem.textContent = `Title: ${event.target.title.value} Author: ${event.target.author.value} Category: ${event.target.category.value}`;
+    resultList.appendChild(newListItem);
+    form.reset();
+  }
+
+  
